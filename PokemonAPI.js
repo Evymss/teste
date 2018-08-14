cac452a0e0ee08e1cf7756e7bf430f87f7b0c71d
Evymss <42047955+Evymss@users.noreply.github.com>
2018-08-14 18:30:49 -0300

    Update PokemonAPI.js

--- PokemonAPI.js
+++ PokemonAPI.js
@@ -8,37 +8,12 @@ xhr.addEventListener("load", function(){
 
 	let $containerCartas = document.getElementById('container')
 	
+	
 	for(let i=0; i<arrayDePokemons.length; i++){
 
-			   var $image = document.createElement('img');
+		
+	       var $nome= document.createTextNode("Nome:"+ arrayDePokemons[i].name);	  
                var $li = document.createElement('li');
-               image.value = .push(arrayDePokemons[i].imageUrl);
-                  $li.appendChild($image);
-
-         /*           // vai criar um atributo com o método createAttribute
-            var $src = document.createAttribute("src"); 
-
-            // vai pegar um elemento do array, sendo que o index é o numero randomico
-            $src.value = links[numRandom];
-            
-            // O método createElement vai criar um elemento <img>
-            var $img = document.createElement('img');
-            
-            // O atributo vai ser setado na imagem com setAttributeNode
-            $img.setAttributeNode($src);
-
-            // Novamente o appendChild. Agora vai add o <img> dentro do <span>
-            $container.appendChild($img);
-        });
-
-        // Um listener é adicionando no botão, esse método recebe dois parametros. O primeiro é o tipo do evento
-        // que no caso é click (não tem On, apenas click) e o segundo é uma função anonima.
-        $botao.addEventListener('click',function(){
-
-            links.push($URL.value); */
-
-
-				 var $nome= document.createTextNode("Nome:"+ arrayDePokemons[i].name);
                   $li.appendChild($nome);
                 var $artist= document.createTextNode("Artista:"+ arrayDePokemons[i].artist);
                 $li.appendChild($artist);
