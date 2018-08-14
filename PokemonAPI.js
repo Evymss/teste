f42e53bd4d120d996d81bec27059679873f80fa5
Evymss <42047955+Evymss@users.noreply.github.com>
2018-08-14 19:44:48 -0300

    Add files via upload
    Atualizado

--- PokemonAPI.js
+++ PokemonAPI.js
@@ -7,32 +7,29 @@ xhr.addEventListener("load", function(){
 	arrayDePokemons = JSON.parse(response).cards;
 
 	let $containerCartas = document.getElementById('container')
-	
-	
-	
+	let $img = document.createElement('img')
+	let $src = document.createAttribute('src')
 	
 	for(let i=0; i<arrayDePokemons.length; i++){
 
-			  
-            
-				var $nome= document.createTextNode("Nome:"+ arrayDePokemons[i].name);
-                    var $li = document.createElement('li'); 
+			var $li = document.createElement('li'); 	
+
+			$src.value = arrayDePokemons[i].imageUrl;
+				$img.setAttributeNode($src);
+
+					$li.appendChild($img);
+                
+                var $nome= document.createTextNode("Nome:"+ arrayDePokemons[i].name);
                     $li.appendChild($nome);
                 var $artist= document.createTextNode("Artista:"+ arrayDePokemons[i].artist);
-                $li.appendChild($artist);
+                	$li.appendChild($artist);
                 var $types = document.createTextNode("Tipo:"+arrayDePokemons[i].types);
-                $li.appendChild($types);
+               		 $li.appendChild($types);
                 var $text = document.createTextNode("Texto:"+arrayDePokemons[i].text);
-                $li.appendChild($text); 
-
-let $img = document.createElement('img')
-	let $src = document.createAttribute('src')
-				$src.value = arrayDePokemons[i].imageUrl;
+               	 	$li.appendChild($text); 
 
 
-				$img.setAttributeNode(src);
-
-			$li.appendChild($img);
+				
                 $containerCartas.appendChild($li);
 
 
